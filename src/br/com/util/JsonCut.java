c33536272ca5272edb41344a2774bd26d7cedf5d
Marcus Vieira <marcusvmvieira@gmail.com>
2022-12-28 14:09:55 -0300

    Criação de classe JsonCut no pacote util, melhorando o acoplamento

---
+++ src/br/com/util/JsonCut.java
@@ -0,0 +1,51 @@
+package br.com.util;
+
+public class JsonCut {
+    //Declaração de atributos privados
+    private String jsonInicial;
+    private String jsonActual;
+    private String[] jsonForecast;
+
+    /**
+     * Método que realiza o corte do json para os trechos que compõe o clima atual o forecast; 
+     */
+    private void jsonCut(String json){
+        //Corte para limpeza e separação dos conteúdos actual e forecast
+        String[] corteJsonInicial = (json.split("\"results\":"))[1].split(",\"forecast\":");
+
+        //Separação do trecho do clima Actual e atribuição ao atributo específico
+        this.jsonActual = corteJsonInicial[0] + "}";
+
+        //
+        String[] corteForecast1 = corteJsonInicial[1].split("],\"cref\"");
+
+        String corteForecast2 = corteForecast1[0].replace("[", "");
+
+        String[] corteForecast3 = corteForecast2.split("\\},\\{");
+
+        corteForecast3[0] = corteForecast3[0].replaceAll("\\{", "");
+        corteForecast3[9] = corteForecast3[9].replaceAll("\\}", "");
+
+        for(int i = 0; i < corteForecast3.length; i++){
+            corteForecast3[i] = "{" + corteForecast3[i] + "}";
+        }
+
+        this.jsonForecast = corteForecast3;
+    }
+
+
+    public String getJsonInicial(String json) {
+        jsonCut(json);
+        return jsonInicial;
+    }
+
+    public String getJsonActual(String json) {
+        jsonCut(json);
+        return jsonActual;
+    }
+
+    public String[] getJsonForecast(String json) {
+        jsonCut(json);
+        return jsonForecast;
+    }
+}
